61dbb3b2c6fad86fe66b975696c4c4cc7d5609b4
parawi <panjiragilz@gmail.com>
2025-10-28 00:03:04 +0800

    feat: disable unused const

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,13 +6,13 @@
   import { Runatics } from 'runatics';
   
   let { children, data }: LayoutProps = $props();
-  const analyticsId = data.ANALYTICS_ID as string;
+  // const analyticsId = data.ANALYTICS_ID as string;
   
   // console.log('analyticsId', data.ANALYTICS_ID);
   onMount(modeobserver);
 </script>
 
-{#if analyticsId}
+<!-- {#if analyticsId}
   <Runatics {analyticsId} />
-{/if}
+{/if} -->
 {@render children()}
