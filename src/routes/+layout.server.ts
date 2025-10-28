b4482c6b454fcf6a731b5cf3f74f4505b152a39c
parawi <panjiragilz@gmail.com>
2025-10-28 10:28:59 +0800

    feat: disable unused lines

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,14 +3,22 @@ import type { LayoutServerLoad } from './$types';
 const json = (r: Response) => r.json();
 // export const prerender = true;
 
-export const load: LayoutServerLoad = async ({ fetch }) => {
-  try {
-    const posts = await fetch('/api/posts').then(json);
-    // console.log('posts: ', posts);
-    return { 
-      // ANALYTICS_ID, 
-      posts };
-  } catch (error) {
-    console.error(`Error in load function for /: ${error}`);
-  }
+// export const load: LayoutServerLoad = async ({ fetch }) => {
+//   try {
+//     const posts = await fetch('/api/posts').then(json);
+//     // console.log('posts: ', posts);
+//     return { 
+//       // ANALYTICS_ID, 
+//       posts };
+//   } catch (error) {
+//     console.error(`Error in load function for /: ${error}`);
+//   }
+// };
+
+export const load = () => {
+  return {
+    posts: {
+      adminDashboard: [],
+    },
+  };
 };
